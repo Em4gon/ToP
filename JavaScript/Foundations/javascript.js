1d60d0ee329af95a277f92d03c2dcef248a31a9e
Em4gon <em4gon@gmail.com>
2022-05-21 02:44:39 -0300

    started javascript basics

---
+++ JavaScript/Foundations/javascript.js
@@ -0,0 +1,19 @@
+
+// Your JavaScript goes here!
+console.log("Hello, World!")
+
+let message = 'Hello friend'; //shows the string 'Hello' in the variable named message
+
+alert(message); //shows the variable content
+
+let user = 'John', 
+    age = 25
+    hola = 'Hola';
+
+message = 'Hola como va?';
+hola = message;
+alert(message);
+
+// There are two limitations on variable names in JavaScript:
+// The name must contain only letters, digits, or the symbols $ and _.
+// The first character must not be a digit.
